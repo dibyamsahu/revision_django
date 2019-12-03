a95efb8935da775ff83a1ab94b05c4bf44f0e5e1
dibyamsahu <dibyam008@gmail.com>
2019-12-03 16:27:33 +0530

    new advance level is added class based view

---
+++ advance_level/school_project/basic_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=256)
+    principal = models.CharField(max_length=256)
+    address = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=256)
+    school = models.ForeignKey(School,related_name="students")
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+    
