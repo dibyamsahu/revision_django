00c7c8117fe2810e44f7f8334b161fd76f2cb8fc
dibyamsahu <dibyam008@gmail.com>
2019-12-07 20:13:10 +0530

    school project added

--- advance_level/demo_project/demo_app/models.py
+++ advance_level/demo_project/demo_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 # Create your models here.
 
 class School(models.Model):
@@ -11,13 +11,13 @@ class School(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("basic_app:detail", kwargs={"pk": self.pk})
+        return reverse("demo_app:detail", kwargs={"pk": self.pk})
     
-
+    
 class Student(models.Model):
     name = models.CharField(max_length=256)
-    school = models.ForeignKey(School,related_name="students")
     age = models.PositiveIntegerField()
+    school = models.ForeignKey(School,related_name="students")
 
     def __str__(self):
         return self.name
--- advance_level/demo_project/demo_app/views.py
+++ advance_level/demo_project/demo_app/views.py
@@ -1,33 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
-from basic_app.models import School
-from django.core.urlresolvers import reverse_lazy
+from demo_app.models import School
+from django.urls import reverse_lazy
 # Create your views here.
+
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
 
 class SchoolListView(ListView):
     context_object_name = "schools"
     model = School
 
-class SchoolDetailView(DetailView):
-    context_object_name = "school_detail"    
-    model = School
-    template_name = "basic_app/school_detail.html"
 
+class SchoolDetailView(DetailView):
+    context_object_name = "school_detail"
+    model = School
+    template_name = "demo_app/school_detail.html"
 
 class SchoolCreateView(CreateView):
     model = School
-    fields = ("name", "principal", "address")
-    
+    fields = ("name","principal","address")
+
 class SchoolUpdateView(UpdateView):
-    model = School
+    model=School
     fields = ("name","principal")
 
 class SchoolDeleteView(DeleteView):
     model = School
-    success_url = reverse_lazy("basic_app:list")
-
-
+    success_url = reverse_lazy("demo_app:list")
 
